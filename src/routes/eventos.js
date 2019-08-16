786b8bf8e5f8703cd95b10e773f52808ea8ead5d
manu <manguher@gmail.com>
2019-08-16 00:26:49 -0400

    base de datos eventos mysql

--- src/routes/eventos.js
+++ src/routes/eventos.js
@@ -1,64 +1,90 @@
 const { Router } = require('express');
-var jsonfile = require('jsonfile');
 const _ = require('underscore');
+var mysql = require('mysql');
 const router = Router();
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://admin:admindb_weblg@cluster0-rzabg.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-var ObjectID = require('mongodb').ObjectID;
 
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'lucianog_bdweb'
+});
 
 router.get('/', (req, res) => {
 
-    MongoClient.connect(uri, function (err, db) {
-
-        if (err) res.status(500).send(err);
-        var dbo = db.db("dbweblg");
-        var today = new Date().toLocaleDateString('es-CL', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric'
-        });
-
-        dbo.collection("eventos").find({ "fecha": { $gte: new Date(today) } })
-            .toArray(function (err, result) {
-                if (err) res.status(500).send(err);
-                res.send(result)
-                db.close();
-            });
-    });
+    var query = connection.query(
+        'SELECT * FROM eventos where DATE(evento_fecha) >= DATE(NOW())', function (error, result) {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(result);
+            }
+        }
+    );
 });
 
 router.put('/:id', (req, res) => {
 
     const { id } = req.params;
-    const { title, fecha, direccion, description } = req.body;
+    const { title, fecha, hora, direccion, description } = req.body;
 
-    if (id && title && fecha && direccion) {
+    // update eventos
+    let sql = `UPDATE eventos
+    SET evento_titulo = ?,
+    evento_fecha = ?,
+    evento_hora = ?,
+    evento_direccion = ?,
+    evento_descripcion = ?
+    WHERE evento_id = ?`;
+
+    let data = [];
+    data.push(title)
+    data.push(fecha)
+    data.push(hora)
+    data.push(direccion)
+    data.push(description)
+    data.push(id)
+
+    if (id && title && fecha && hora && direccion) {
+        connection.query(sql, data, (error, results, fields) => {
+            if (error) {
+                return console.error(error.message);
+            }
+            console.log('Rows affected:', results.affectedRows);
+        });
+    }
+    else {
+        res.send("debe completar los campos requeridos");
+    }
+});
+
+router.post('/addevento', (req, res) => {
+
+    const { title, fecha, hora, direccion, description } = req.body;
+    if (title && fecha && hora && direccion) {
         MongoClient.connect(uri, function (err, db) {
 
             if (err) res.status(500).send('error de base de datos: ' + err);
             var dbo = db.db("dbweblg");
 
-            dbo.collection("eventos").updateOne(
-                { "_id": ObjectID(id) },
+            dbo.collection("eventos").insertOne(
                 {
-                    $set:
-                    {
-                        "title": title,
-                        "fecha": new Date(fecha),
-                        "direccion": direccion,
-                        "description": description
-                    }
+                    "title": title,
+                    "fecha": new Date(fecha),
+                    "hora": hora,
+                    "direccion": direccion,
+                    "description": description
                 },
                 function (err, result) {
-                    if (err) res.status(500).send("error al actualizar los datos : " + err);
-                    res.status(200).send('evento editado correctamente')
+                    if (err) res.status(500).send("error al insertar evento: " + err);
+                    res.status(200).send('evento guardado correctamente')
                     db.close();
-                }
-            )
+                })
         });
+    }
+    else {
+        res.status(500).send('debe completar campos' + err);
     }
 });
 module.exports = router;
